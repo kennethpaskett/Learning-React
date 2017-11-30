1a553f9dbde5d528e6db5d8f0bb167a10cfe83b0
Kenneth Paskett <kenneth.paskett@gmail.com>
2017-11-30 09:20:10 -0700

    Update index files to include code for utilizing JSX and Babel

--- New Project/index.js
+++ New Project/index.js
@@ -1,16 +1,6 @@
-const { createElement } = React
 const { render } = ReactDOM
 
-const title = createElement(
-  'h1',
-  {
-    id: 'title',
-    className: 'header',
-  },
-  'Hello World'
-)
-
 render(
-  title,
+  <h1 id="title" className="header" style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}>Hello World</h1>,
   document.getElementById('react-container')
 )
